50c809957832e4d89d64abb2cc9dd0c0806b8744
alistairkeys <alistair_keys@hotmail.com>
2021-12-10 22:25:54 +0000

    Day 7 part 2

--- src/day07.nim
+++ src/day07.nim
@@ -1,4 +1,4 @@
-import std/[sequtils, strutils, algorithm]
+import std/[sequtils, strutils, algorithm, math, stats]
 
 # https://adventofcode.com/2021/day/7
 
@@ -6,12 +6,23 @@ proc part1(filename: string): int =
   let values = filename.readFile.split(",").map(parseInt).sorted
   values.foldl(a + abs(b - values[values.len div 2]), 0)
 
-#proc part2(filename: string): int =
-  #result = ??
+proc part2(filename: string): int =
+
+  let values = filename.readFile.split(",").map(parseInt).sorted
+
+  proc totalFuel(mean: int): int =
+    # Uses the Gauss formula for summing consecutive numbers:
+    # https://nrich.maths.org/2478
+    for v in values:
+      let dist = abs(v - mean)
+      result += (dist * (dist + 1)) div 2
+
+  let meanValue = mean values
+  result = min(totalFuel(meanValue.floor.int), totalFuel(meanValue.ceil.int))
 
 when isMainModule:
   doAssert part1("../data/day07_example.txt") == 37
   echo part1("../data/day07_input.txt")
 
-  #doAssert part2("../data/day07_example.txt") == 168
-  #echo part2("../data/day07_input.txt")
+  doAssert part2("../data/day07_example.txt") == 168
+  echo part2("../data/day07_input.txt")
