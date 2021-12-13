bd5d32050bd0b51c95d4f496b92980004a8c8270
alistairkeys <alistair_keys@hotmail.com>
2021-12-13 01:15:26 +0000

    Day 10, part 2
    I wanted to push it further but the compiler started to give me weird
    errors!

--- src/day10.nim
+++ src/day10.nim
@@ -1,29 +1,42 @@
-import std/[sequtils, tables]
+import std/[sequtils, tables, algorithm, math, sugar]
 
 # https://adventofcode.com/2021/day/10
 
-proc part1(filename: string): int =
+const expectedToken = {'(': ')', '[': ']', '{': '}', '<': '>'}.toTable
 
+func isOpening(ch: char): bool = expectedToken.hasKey ch
+func isClosing(ch: char): bool = ch in expectedToken.values.toSeq
+
+type
+  ParsedLine = object
+    stack: seq[char]
+    badTokenScore: int
+
+proc parseLine(line: string): ParsedLine =
   const badTokenScore = {')': 3, ']': 57, '}': 1197, '>': 25137}.toTable
-  const expectedToken = {'(': ')', '[': ']', '{': '}', '<': '>'}.toTable
-  func isOpening(ch: char): bool = expectedToken.hasKey ch
-  func isClosing(ch: char): bool = ch in expectedToken.values.toSeq
+  for token in line:
+    if isOpening token:
+      result.stack.add token
+    elif isClosing(token) and token != expectedToken.getOrDefault(
+        result.stack.pop, '\0'):
+      result.badTokenScore = badTokenScore[token]
+      break
+  result.stack.reverse
 
-  for l in filename.lines:
-    var stack: seq[char]
-    for token in l.toSeq:
-      if isOpening token:
-        stack.add token
-      elif isClosing(token) and token != expectedToken.getOrDefault(stack.pop, '\0'):
-        inc result, badTokenScore[token]
-        break
+proc part1(filename: string): int =
+  sum filename.lines.toSeq.mapIt(it.parseLine.badTokenScore)
 
-#proc part2(filename: string): int =
-  #result = ??
+proc part2(filename: string): int =
+  const incompleteTokenScores = {')': 1, ']': 2, '}': 3, '>': 4}.toTable
+  var incompleteLineScores = collect:
+    for parsedLine in filename.lines.toSeq.map(parseLine).filterIt(
+        it.badTokenScore == 0):
+      parsedLine.stack.foldl(a * 5 + incompleteTokenScores[expectedToken[b]], 0)
+  incompleteLineScores.sorted[incompleteLineScores.len div 2]
 
 when isMainModule:
   doAssert part1("../data/day10_example.txt") == 26397
   echo part1("../data/day10_input.txt")
 
-  #doAssert part2("../data/day10_example.txt") == ??
-  #echo part2("../data/day10_input.txt")
+  doAssert part2("../data/day10_example.txt") == 288957
+  echo part2("../data/day10_input.txt")
